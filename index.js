7db585b3f5c0e6c704764df3d6c3b0413e280336
Louis T <louist@ltdev.im>
2016-12-31 08:05:33 +0000

    Fix the reject calls in ReadFrom.prototype.file

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /*!
- * Read text content from different endpoints. - https://ltdev.im/ - Copyright (c) 2016 Louis T.   
+ * Read text content from different endpoints. - https://ltdev.im/ - Copyright (c) 2016 Louis T.
  * Licensed under MIT license https://raw.githubusercontent.com/LouisT/ReadFrom/master/LICENSE
  */
 module.exports = require('./lib/readfrom.js');
